65facddb20e406730f09a9231d1ad2b6b0d9e619
Dhruv Rajan <dhruv@krishnaprem.com>
2018-11-26 17:34:41 -0600

    added augmented encoder model for attention

--- models/attention.py
+++ models/attention.py
@@ -6,6 +6,72 @@ from torch.optim import Adam
 
 from models.LSTM import RNNEncoder, EmbeddingLayer, LSTMTrainedModel
 
+class RNNEncoder(nn.Module):
+    # Parameters: input size (should match embedding layer), hidden size for the LSTM, dropout rate for the RNN,
+    # and a boolean flag for whether or not we're using a bidirectional encoder
+    def __init__(self, input_size, hidden_size, dropout, bidirect):
+        super(RNNEncoder, self).__init__()
+        self.bidirect = bidirect
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.reduce_h_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
+        self.reduce_c_W = nn.Linear(hidden_size * 2, hidden_size, bias=True)
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers=1, batch_first=True,
+                           dropout=dropout, bidirectional=self.bidirect)
+        self.init_weight()
+
+    # Initializes weight matrices using Xavier initialization
+    def init_weight(self):
+        nn.init.xavier_uniform_(self.rnn.weight_hh_l0, gain=1)
+        nn.init.xavier_uniform_(self.rnn.weight_ih_l0, gain=1)
+        if self.bidirect:
+            nn.init.xavier_uniform_(self.rnn.weight_hh_l0_reverse, gain=1)
+            nn.init.xavier_uniform_(self.rnn.weight_ih_l0_reverse, gain=1)
+        nn.init.constant_(self.rnn.bias_hh_l0, 0)
+        nn.init.constant_(self.rnn.bias_ih_l0, 0)
+        if self.bidirect:
+            nn.init.constant_(self.rnn.bias_hh_l0_reverse, 0)
+            nn.init.constant_(self.rnn.bias_ih_l0_reverse, 0)
+
+    def get_output_size(self):
+        return self.hidden_size * 2 if self.bidirect else self.hidden_size
+
+    def sent_lens_to_mask(self, lens, max_length):
+        return torch.from_numpy(np.asarray(
+            [[1 if j < lens.data[i].item() else 0 for j in range(0, max_length)] for i in range(0, lens.shape[0])]))
+
+    # embedded_words should be a [batch size x sent len x input dim] tensor
+    # input_lens is a tensor containing the length of each input sentence
+    # Returns output (each word's representation), context_mask (a mask of 0s and 1s
+    # reflecting where the model's output should be considered), and h_t, a *tuple* containing
+    # the final states h and c from the encoder for each sentence.
+    def forward(self, embedded_words, input_lens):
+        # Takes the embedded sentences, "packs" them into an efficient Pytorch-internal representation
+        packed_embedding = nn.utils.rnn.pack_padded_sequence(embedded_words, input_lens, batch_first=True)
+        # Runs the RNN over each sequence. Returns output at each position as well as the last vectors of the RNN
+        # state for each sentence (first/last vectors for bidirectional)
+        output, hn = self.rnn(packed_embedding)
+        # Unpacks the Pytorch representation into normal tensors
+        output, sent_lens = nn.utils.rnn.pad_packed_sequence(output)
+        max_length = input_lens.data[0].item()
+        context_mask = self.sent_lens_to_mask(sent_lens, max_length)
+
+        # Grabs the encoded representations out of hn, which is a weird tuple thing.
+        # Note: if you want multiple LSTM layers, you'll need to change this to consult the penultimate layer
+        # or gather representations from all layers.
+        if self.bidirect:
+            h, c = hn[0], hn[1]
+            # Grab the representations from forward and backward LSTMs
+            h_, c_ = torch.cat((h[0], h[1]), dim=1), torch.cat((c[0], c[1]), dim=1)
+            # Reduce them by multiplying by a weight matrix so that the hidden size sent to the decoder is the same
+            # as the hidden size in the encoder
+            new_h = self.reduce_h_W(h_)
+            new_c = self.reduce_c_W(c_)
+            h_t = (new_h, new_c)
+        else:
+            h, c = hn[0][0], hn[1][0]
+            h_t = (h, c)
+        return (output, context_mask, h_t)
 
 
 
@@ -45,7 +111,12 @@ class AttentionRNNDecoder(nn.Module):
 
 
 
-def _run_encoder(input_tensor, model_input_emb)
+def encode_input_for_decoder(x_tensor, inp_lens_tensor, model_input_emb, model_enc):
+    input_emb = model_input_emb.forward(x_tensor)
+    (enc_output_each_word, enc_context_mask, enc_final_states) = model_enc.forward(input_emb, inp_lens_tensor)
+    enc_final_states_reshaped = (enc_final_states[0].unsqueeze(0), enc_final_states[1].unsqueeze(0))
+    return (enc_output_each_word, enc_context_mask, enc_final_states_reshaped)
+
 
 
 def example(input_tensor, output_tensor, input_lens_tensor,
