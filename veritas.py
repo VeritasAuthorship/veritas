0376acb0becf009773a1a092aa90953343bd86cf
Dhruv Rajan <dhruv@krishnaprem.com>
2018-12-03 19:52:56 -0600

    adding option for SPOOKY LSTM_ATTN

--- veritas.py
+++ veritas.py
@@ -16,6 +16,7 @@ from models.baseline import *
 from models.sklearn_baselines import sklearn_train
 from models.LSTM import *
 
+
 # Read in command line arguments to the system
 def arg_parse():
     parser = argparse.ArgumentParser(description='trainer.py')
@@ -24,7 +25,6 @@ def arg_parse():
     parser.add_argument('--train_path', type=str, default='data/british/', help='Path to the training set')
     parser.add_argument('--test_path', type=str, default='data/gut-test/british', help='Path to the test set')
 
-
     # Seq-2-Seq args
     parser.add_argument('--reverse_input', type=bool, default=False)
     parser.add_argument("--batch_size", type=int, default=1, help="batch size for encoder training")
@@ -32,12 +32,15 @@ def arg_parse():
     parser.add_argument('--rnn_dropout', type=float, default=0.2, help="dropout for RNN")
     parser.add_argument('--bidirectional', type=bool, default=True, help="lstm birectional or not")
     parser.add_argument('--hidden_size', type=int, default=200, help='hidden state dimensionality')
-    parser.add_argument('--word_vecs_path_input', type=str, default='data/glove.6B.300d.txt', help='path to word vectors file')
-    parser.add_argument('--word_vecs_path', type=str, default='data/glove.6B.300d-relativized.txt', help='path to word vectors file')
+    parser.add_argument('--word_vecs_path_input', type=str, default='data/glove.6B.300d.txt',
+                        help='path to word vectors file')
+    parser.add_argument('--word_vecs_path', type=str, default='data/glove.6B.300d-relativized.txt',
+                        help='path to word vectors file')
     parser.add_argument('--embedding_size', type=int, default=300, help='Embedding size')
 
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     args = arg_parse()
@@ -46,7 +49,7 @@ if __name__ == "__main__":
     if args.model == 'BASELINE':
         # Get books from train path and call baselineb model train function
         if (args.train_type == 'GUTENBERG'):
-            train_data, test_data, num_authors =  gutenberg_dataset(args.train_path, args.test_path)
+            train_data, test_data, num_authors = gutenberg_dataset(args.train_path, args.test_path)
             print("training")
             authors = train_baseline(train_data)
             print("testing")
@@ -109,6 +112,23 @@ if __name__ == "__main__":
 
             print("testing")
             trained_model.evaluate(test_data)
+
+        elif args.train_type == "SPOOKY":
+            train_data, test_data, authors = spooky_authorship_data()
+            word_indexer = Indexer()
+            add_dataset_features(train_data, word_indexer)
+            add_dataset_features(test_data, word_indexer)
+
+            relativize(args.word_vecs_path_input, args.word_vecs_path, word_indexer)
+            word_vectors = read_word_embeddings(args.word_vecs_path)
+
+            print("Finished extracting embeddings")
+            print("training")
+            trained_model = train_enc_dec_model(train_data, test_data, authors, word_vectors, args)
+
+            print("testing")
+            trained_model.evaluate(test_data)
+
 
     elif args.model == "KERAS":
         if args.train_type == "GUTENBERG":
