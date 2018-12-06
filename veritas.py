c56384d1af18900a6090d34bdbada32e7940d16e
Dhruv Rajan <dhruv@krishnaprem.com>
2018-12-05 22:12:24 -0600

    changing order of sentencewise run

--- veritas.py
+++ veritas.py
@@ -112,9 +112,11 @@ if __name__ == "__main__":
         data = get_data(args)
         train_data, test_data, authors = data
 
+        flattened_test_data = list(itertools.chain(*test_data)) if args.sentencewise else test_data
+
         word_indexer = Indexer()
         add_dataset_features(train_data, word_indexer)
-        add_dataset_features(test_data, word_indexer)
+        add_dataset_features(flattened_test_data, word_indexer)
 
         if args.train_options == 'POS':
             pretrained = False
@@ -129,7 +131,7 @@ if __name__ == "__main__":
         print("Finished extracting embeddings")
         print("training")
 
-        trained_model = train_lstm_attention_model(train_data, test_data, authors, word_vectors, args)
+        trained_model = train_lstm_attention_model(train_data, flattened_test_data, authors, word_vectors, args)
 
         print("testing")
         trained_model.evaluate(test_data, args)
