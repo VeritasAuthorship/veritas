fcf778c58f8d669e5c394d5e616c513ba08134af
Dhruv Rajan <dhruv@krishnaprem.com>
2018-12-13 12:05:09 -0600

    adding new attention classifier--no RNN decoder

--- veritas.py
+++ veritas.py
@@ -8,6 +8,7 @@ import sys
 
 from models.attention import train_lstm_attention_model
 from models.gru_attention import train_gru_attention_model
+from models.du_attn_classifier import train_du_attention_model
 from utils import *
 from gutenberg_data import *
 from spooky_authorship import spooky_authorship_data
@@ -16,7 +17,7 @@ from spooky_authorship import spooky_authorship_data
 from models.baseline import *
 # from models.sklearn_baselines import sklearn_train
 from models.LSTM import *
-from models.attention import train_lstm_attention_model
+# from models.attention import train_lstm_attention_model
 # sys.path.append("./models")
 from models.baseline import *
 from models.sentence_wise_classification import *
@@ -43,7 +44,7 @@ def arg_parse():
     parser.add_argument('--emb_dropout', type=float, default=0.2, help="dropout for embedding layer")
     parser.add_argument('--rnn_dropout', type=float, default=0.2, help="dropout for RNN")
     parser.add_argument('--bidirectional', type=bool, default=True, help="lstm birectional or not")
-    parser.add_argument('--hidden_size', type=int, default=200, help='hidden state dimensionality')
+    parser.add_argument('--hidden_size', type=int, default=300, help='hidden state dimensionality')
     parser.add_argument('--word_vecs_path_input', type=str, default='data/glove.6B.300d.txt',
                         help='path to word vectors file')
     parser.add_argument('--word_vecs_path', type=str, default='data/glove.6B.300d-relativized.txt',
@@ -141,6 +142,34 @@ if __name__ == "__main__":
         trained_model.evaluate(test_data, args)
 
     elif args.model == "GRU_ATTN":
+        data = get_data(args)
+        train_data, test_data, authors = data
+
+        flattened_test_data = list(itertools.chain(*test_data)) if args.sentencewise else test_data
+
+        word_indexer = Indexer()
+        add_dataset_features(train_data, word_indexer)
+        add_dataset_features(flattened_test_data, word_indexer)
+
+        if args.train_options == 'POS':
+            pretrained = False
+            word_indexer.get_index(PAD_SYMBOL)
+            word_indexer.get_index(UNK_SYMBOL)
+            word_vectors = WordEmbeddings(word_indexer, None)
+        else:
+            pretrained = True
+            relativize(args.word_vecs_path_input, args.word_vecs_path, word_indexer)
+            word_vectors = read_word_embeddings(args.word_vecs_path)
+
+        print("Finished extracting embeddings")
+        print("training")
+
+        trained_model = train_gru_attention_model(train_data, flattened_test_data, authors, word_vectors, args, pretrained=pretrained)
+
+        print("testing")
+        trained_model.evaluate(test_data, args)
+
+    elif args.model == "DU_ATTN":
         data = get_data(args)
         train_data, test_data, authors = data
 
